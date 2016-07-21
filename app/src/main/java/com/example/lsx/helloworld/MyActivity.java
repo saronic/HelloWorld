6d427dcdf22283debea65fdedab15a4f6f296276
lsx <lsx@163.com>
2016-07-21 15:55:56 +0800

    button可以log

--- app/src/main/java/com/example/lsx/helloworld/MyActivity.java
+++ app/src/main/java/com/example/lsx/helloworld/MyActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -24,6 +26,13 @@ public class MyActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        Button button = (Button) findViewById(R.id.content_my_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("lsx", "xxxxxxxxxxxxxxxxxxxx");
             }
         });
     }
